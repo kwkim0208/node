f157049ba493ebf207b637056e1da9e9c0e4d379
kwkim <ruddn0208@gmail.com>
2020-09-12 23:37:14 +0900

    Initial commit

---
+++ ch03/os.js
@@ -0,0 +1,4 @@
+const os =require('os');
+console.log('운영체체 정보==============>')
+console.log('os.arch()'+os.arch());
+console.log('os.platform()'+os.platform());
---
+++ ch04/server1-1.js
@@ -0,0 +1,13 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8' });
+    res.write('<h1>Hello</h1>');
+    res.end('<p>Hello server</p>');
+});
+server.listen(8080);
+server.on('listening',()=>{
+    console.log('8080번 포트에서 서버가 대기중입니다.');
+});
+server.on('error',(error)=>{
+    console.error(error);
+})
